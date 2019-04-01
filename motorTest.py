1c318c7ef8ac8e05350207407376bbe247fc04d9
chillchillchou <hc2446@nyu.edu>
2019-03-31 23:25:08 -0400

    add led

--- motorTest.py
+++ motorTest.py
@@ -1,8 +1,13 @@
-from gpiozero import OutputDevice
-from gpiozero import Button
+from gpiozero import OutputDevice, Button, LED
+
 motor = OutputDevice(3)
-button = OutputDevice(14)
+button = Button(14)
+led = LED(4)
 
 button.wait_for_press()
 print("you pressed the button")
 motor.on()
+led.on()
+button.wati_for_release()
+motor.off()
+led.off()
