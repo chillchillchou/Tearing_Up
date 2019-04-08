bdb130d7b92796c6fcb3884cf9b7952d421e6974
chillchillchou <huiyiche@usc.edu>
2019-04-08 02:25:45 -0400

    add test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+from gpiozero import OutputDevice, Button, LED
+motor = OutputDevice(4)
+button = Button(14)
+led = LED(3)
+
+
+while True:
+    button.wait_for_press()
+    motor.on()
+    print("turn on motor")
+    led.on()
+    sleep(20)
+    led.off()
+    motor.off()
+    print("turn off motor")
